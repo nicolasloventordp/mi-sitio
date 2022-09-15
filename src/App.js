4ec4acff784bcaa5bd3de8259b79ba17c0f2ec0e
Nicolas Lo Vento <nicolaslovento.94@gmail.com>
2022-09-15 12:39:49 -0300

    some changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+//import Home from './components/Home'
+//import Acerca from './components/Acerca'
+//import Contacto from './components/Contacto'
+//import Footer from './components/Footer'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+import { useEffect } from 'react'
+import './App.css'
 
 function App() {
+  useEffect( () => {
+    Aos.init({
+      duration: 2000
+    })
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      {/*<Home />
+      <Acerca />
+      <Contacto />
+  <Footer />*/}
     </div>
   );
 }
